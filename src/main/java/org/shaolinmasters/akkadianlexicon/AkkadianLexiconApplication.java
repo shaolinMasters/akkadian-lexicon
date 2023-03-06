beaf3f580716078bf4e5cb69ff26e75fd3c2a65d
marinchuck <marincsakt@gmail.com>
2023-03-06 10:16:29 +0100

    Initial commit

---
+++ src/main/java/org/shaolinmasters/akkadianlexicon/AkkadianLexiconApplication.java
@@ -0,0 +1,13 @@
+package org.shaolinmasters.akkadianlexicon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AkkadianLexiconApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AkkadianLexiconApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/shaolinmasters/akkadianlexicon/AkkadianLexiconApplicationTests.java
@@ -0,0 +1,13 @@
+package org.shaolinmasters.akkadianlexicon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AkkadianLexiconApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
