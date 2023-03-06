c9cb350bd223e0b55966945cc2a4149c1cef32f3
marinchuck <marincsakt@gmail.com>
2023-03-06 11:35:05 +0100

    Update AkkadianLexiconApplication.java

--- src/main/java/org/shaolinmasters/akkadianlexicon/AkkadianLexiconApplication.java
+++ src/main/java/org/shaolinmasters/akkadianlexicon/AkkadianLexiconApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class AkkadianLexiconApplication {
-  // on dev
   public static void main(String[] args) {
     SpringApplication.run(AkkadianLexiconApplication.class, args);
   }
