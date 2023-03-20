39a4d2ce30f94f1a6c139a0ec1d43e5374a41a2a
marinchuck <marincsakt@gmail.com>
2023-03-20 17:35:59 +0100

    Update schema.sql

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,6 +1,7 @@
 USE akkadian_lexicon;
 DROP TABLE IF EXISTS `word_source`;
 DROP TABLE IF EXISTS `adjective`;
+DROP TABLE IF EXISTS `pronoun`;
 DROP TABLE IF EXISTS `verb`;
 DROP TABLE IF EXISTS `noun`;
 DROP TABLE IF EXISTS `source`;
@@ -11,7 +12,7 @@ DROP TABLE IF EXISTS `word`;
 
 CREATE TABLE `king` (
                         `id` bigint NOT NULL AUTO_INCREMENT,
-                        `king_name` varchar(255) COLLATE utf8mb4_0900_as_cs NOT NULL,
+                        `name` varchar(255) COLLATE utf8mb4_0900_as_cs NOT NULL,
                         `regnal_year_from` date DEFAULT NULL,
                         `regnal_year_to` date DEFAULT NULL,
                         PRIMARY KEY (`id`)
@@ -21,8 +22,8 @@ CREATE TABLE `king` (
 
 CREATE TABLE `word` (
                         `id` bigint NOT NULL AUTO_INCREMENT,
-                        `nominative` varchar(255) COLLATE utf8mb4_0900_as_cs NOT NULL,
                         `meaning` varchar(255) COLLATE utf8mb4_0900_as_cs NOT NULL,
+                        `nominative` varchar(255) COLLATE utf8mb4_0900_as_cs NOT NULL,
                         `sumerian_form` varchar(255) COLLATE utf8mb4_0900_as_cs DEFAULT NULL,
                         PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_as_cs;
@@ -30,11 +31,10 @@ CREATE TABLE `word` (
 -- akkadian_lexicon.pronoun definition
 
 CREATE TABLE `pronoun` (
-  `id` bigint NOT NULL,
-  PRIMARY KEY (`id`),
-  CONSTRAINT `FKah63mav2v9c1qjo9j14xx39wd` FOREIGN KEY (`id`) REFERENCES `word` (`id`)
+                           `id` bigint NOT NULL,
+                           PRIMARY KEY (`id`),
+                           CONSTRAINT `FKah63mav2v9c1qjo9j14xx39wd` FOREIGN KEY (`id`) REFERENCES `word` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_as_cs;
-
 
 -- akkadian_lexicon.adjective definition
 
@@ -43,8 +43,6 @@ CREATE TABLE `adjective` (
                              PRIMARY KEY (`id`),
                              CONSTRAINT `FK99j7eg11fxd0uoxf194usp7x0` FOREIGN KEY (`id`) REFERENCES `word` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_as_cs;
-
-
 
 -- akkadian_lexicon.noun definition
 
@@ -58,27 +56,25 @@ CREATE TABLE `noun` (
 
 CREATE TABLE `source` (
                           `id` bigint NOT NULL AUTO_INCREMENT,
+                          `bibliography` varchar(255) COLLATE utf8mb4_0900_as_cs NOT NULL,
+                          `catalogue_ref` varchar(255) COLLATE utf8mb4_0900_as_cs NOT NULL,
                           `text` mediumtext COLLATE utf8mb4_0900_as_cs NOT NULL,
-                          `source_title` varchar(255) COLLATE utf8mb4_0900_as_cs NOT NULL,
+                          `title` varchar(255) COLLATE utf8mb4_0900_as_cs NOT NULL,
                           `king_id` bigint DEFAULT NULL,
                           PRIMARY KEY (`id`),
                           KEY `FKfgi7jq0tm9s4uummju1hwais4` (`king_id`),
                           CONSTRAINT `FKfgi7jq0tm9s4uummju1hwais4` FOREIGN KEY (`king_id`) REFERENCES `king` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_as_cs;
 
-
-
 -- akkadian_lexicon.verb definition
 
 CREATE TABLE `verb` (
-                        `id` bigint NOT NULL,
                         `verbal_stem` varchar(255) COLLATE utf8mb4_0900_as_cs NOT NULL,
                         `vowel_class` varchar(255) COLLATE utf8mb4_0900_as_cs NOT NULL,
+                        `id` bigint NOT NULL,
                         PRIMARY KEY (`id`),
                         CONSTRAINT `FK2phw9aowq1sxv4l1abd9ampy8` FOREIGN KEY (`id`) REFERENCES `word` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_as_cs;
-
-
 
 -- akkadian_lexicon.word_source definition
 
