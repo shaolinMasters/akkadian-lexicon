6693b5cf0b9711f29d0d4c631afeb72954dc73d8
H.M.Dezső <hertermiklosdezso@gmail.com>
2023-03-20 15:09:23 +0100

    first version of welcome UI is done

--- src/main/java/org/shaolinmasters/akkadianlexicon/controllers/WelcomeController.java
+++ src/main/java/org/shaolinmasters/akkadianlexicon/controllers/WelcomeController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class WelcomeController {
-  @GetMapping("/welcome")
+  @GetMapping(path = {"/"})
   String getWelcome() {
     return "welcome";
   }
