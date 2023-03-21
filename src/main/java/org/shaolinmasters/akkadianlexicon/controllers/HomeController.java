f9f4a791a7ebf6a35674cac142714d6b0d2af771
H.M.Dezső <hertermiklosdezso@gmail.com>
2023-03-21 13:09:29 +0100

    making sure everithyng is alright

--- src/main/java/org/shaolinmasters/akkadianlexicon/controllers/HomeController.java
+++ src/main/java/org/shaolinmasters/akkadianlexicon/controllers/HomeController.java
@@ -4,9 +4,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class WelcomeController {
+public class HomeController {
   @GetMapping(path = {"/"})
   String getWelcome() {
-    return "welcome";
+    return "home";
   }
 }
