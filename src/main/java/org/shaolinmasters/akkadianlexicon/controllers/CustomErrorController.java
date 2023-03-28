aee9236b05f64bc0510f7dda12efc5294ae627e5
lengvari <lengvari.tamas@gmail.com>
2023-03-28 17:29:28 +0200

    costum-error-page-2

--- src/main/java/org/shaolinmasters/akkadianlexicon/controllers/CustomErrorController.java
+++ src/main/java/org/shaolinmasters/akkadianlexicon/controllers/CustomErrorController.java
@@ -1,2 +1,24 @@
-package org.shaolinmasters.akkadianlexicon.controllers;public class CustomErrorController {
+package org.shaolinmasters.akkadianlexicon.controllers;
+
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class CustomErrorController implements ErrorController {
+
+  @RequestMapping("/error")
+  public String handleError(HttpServletRequest request, Model model) {
+    Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
+
+    if (status != null) {
+      Integer statusCode = Integer.valueOf(status.toString());
+        model.addAttribute("error", statusCode);
+    }
+    return "error";
+  }
 }
