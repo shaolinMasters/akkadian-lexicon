c97d916abaadc46059b55a0915e66f396f684e33
Csörgő Balázs <115080313+balazscsorgo@users.noreply.github.com>
2023-04-05 13:41:59 +0200

    refactored

--- src/main/java/org/shaolinmasters/akkadianlexicon/listeners/RegistrationListener.java
+++ src/main/java/org/shaolinmasters/akkadianlexicon/listeners/RegistrationListener.java
@@ -147,7 +147,7 @@ public class RegistrationListener implements ApplicationListener<OnRegistrationC
       "<hr>"+
       "<h2>Welcome!</h2>"+
       "<p>"+emailStarterText+emailAddress+emailText+"</p>"+
-      "<button type=\"button\" onclick=\"location.href='http://"+confirmationUrl+"\">"+ "Confirm"+"<button>"+
+      "<button type=\"button\" onclick=\"location.href='http://"+confirmationUrl+"\">"+ "Confirm"+"</button>"+
       "<p1>Thank you,"+
       "</p1>"+
       "<br>"+
