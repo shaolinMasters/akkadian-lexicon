f9688d690a170fb9f647e1ddb3a4d5afc8075704
marinchuck <marincsakt@gmail.com>
2023-04-05 23:29:01 +0200

    implementing error controller

--- src/main/java/org/shaolinmasters/akkadianlexicon/controllers/CustomErrorController.java
+++ src/main/java/org/shaolinmasters/akkadianlexicon/controllers/CustomErrorController.java
@@ -1,21 +1,26 @@
 package org.shaolinmasters.akkadianlexicon.controllers;
 
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @Slf4j
 public class CustomErrorController
-//  implements ErrorController
-{
-  //  @RequestMapping("/error")
-  //  public String handleError(HttpServletRequest request, Model model) {
-  //    logger.error("Requested URI: " + request.getRequestURI());
-  //    Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
-  //    if (status != null) {
-  //      Integer statusCode = Integer.valueOf(status.toString());
-  //      model.addAttribute("error", statusCode);
-  //    }
-  //    return "error";
-  //  }
+  implements ErrorController {
+
+  @RequestMapping("/error")
+  public String handleError(HttpServletRequest request, Model model) {
+    logger.error("Requested URI: " + request.getRequestURI());
+    Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
+    if (status != null) {
+      Integer statusCode = Integer.valueOf(status.toString());
+      model.addAttribute("error", statusCode);
+    }
+    return "error";
+  }
 }
